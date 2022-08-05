f9ead0255155ba15a42a6fe2cc6dbaa5d8afb7cb
Pavel Krigin <pavel.krigin.85@gmail.com>
2022-08-05 21:12:06 +0300

    Initial Commit

---
+++ rgbControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rgbControl
+//
+//  Created by Pavel Krigin on 5.8.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
