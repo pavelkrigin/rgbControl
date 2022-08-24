545257be888b388a922b5e75fd63116246ac72d1
Pavel Krigin <pavel.krigin.85@gmail.com>
2022-08-24 20:44:24 +0300

    fixed_bags_2

--- rgbControl/ViewController.swift
+++ rgbControl/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+//MARK: - Outlets
     @IBOutlet var rgbShow: UIView!
     
     @IBOutlet var redColorMeaning: UILabel!
@@ -19,80 +20,49 @@ final class ViewController: UIViewController {
     @IBOutlet var sliderGreen: UISlider!
     @IBOutlet var sliderBlue: UISlider!
     
+//MARK: - Override Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         rgbShow.layer.cornerRadius = 10
-        setupSliderRed()
-        setupSliderGreen()
-        setupSliderBlue()
-               
+        
+        setupSlider(sliderRed, color: .red)
+        setupSlider(sliderGreen, color: .green)
+        setupSlider(sliderBlue, color: .blue)
+        
+        setupViewColor()
     }
-
+    
+//MARK: - IBA Functions
     @IBAction func sliderRedAction() {
         redColorMeaning.text = String(sliderRed.value)
-        rgbShow.backgroundColor = UIColor(red: CGFloat(sliderRed.value),
-                                          green: CGFloat(sliderGreen.value),
-                                          blue: CGFloat(sliderBlue.value), alpha: 1)
-        
-//        rgbShow.backgroundColor =  rgbShow.backgroundColor?.withAlphaComponent(CGFloat(sliderRed.value))
+        setupViewColor()
     }
     
     @IBAction func sliderGreenAction() {
         greenColorMeaning.text = String(sliderGreen.value)
-        rgbShow.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1)
-        
-//        rgbShow.backgroundColor =  rgbShow.backgroundColor?.withAlphaComponent(CGFloat(sliderGreen.value))
+        setupViewColor()
     }
     
     @IBAction func sliderBlueAction() {
         blueColorMeaning.text = String(sliderBlue.value)
-        rgbShow.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1)
-        
-//        rgbShow.backgroundColor =  rgbShow.backgroundColor?.withAlphaComponent(CGFloat(sliderBlue.value))
+        setupViewColor()
     }
     
     // MARK: - Private Methods
-//    private func sliderActionColor(_: UILabel, _: UISlider) {
-//        UILabel.text = String(UISlider.value)
-//        rgbShow.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: 1)
-//    }
-//    
-//    private func setupSliderColor() {
-//        sliderRed.value = 0
-//        sliderRed.minimumValue = 0
-//        sliderRed.maximumValue = 1
-//        sliderRed.minimumTrackTintColor = .red
-//        sliderRed.maximumTrackTintColor = .gray
-//        sliderRed.thumbTintColor = .red
-//    }
-    
-    private func setupSliderRed() {
-        sliderRed.value = 0
-        sliderRed.minimumValue = 0
-        sliderRed.maximumValue = 1
-        sliderRed.minimumTrackTintColor = .red
-        sliderRed.maximumTrackTintColor = .gray
-        sliderRed.thumbTintColor = .red
-    }
-
-    private func setupSliderGreen() {
-        sliderGreen.value = 0
-        sliderGreen.minimumValue = 0
-        sliderGreen.maximumValue = 1
-        sliderGreen.minimumTrackTintColor = .green
-        sliderGreen.maximumTrackTintColor = .gray
-        sliderGreen.thumbTintColor = .green
-    }
-
-    private func setupSliderBlue() {
-        sliderBlue.value = 0
-        sliderBlue.minimumValue = 0
-        sliderBlue.maximumValue = 1
-        sliderBlue.minimumTrackTintColor = .blue
-        sliderBlue.maximumTrackTintColor = .gray
-        sliderBlue.thumbTintColor = .blue
+    private func setupViewColor() {
+        rgbShow.backgroundColor = UIColor(red: CGFloat(sliderRed.value),
+                                          green: CGFloat(sliderGreen.value),
+                                          blue: CGFloat(sliderBlue.value),
+                                          alpha: 1)
     }
     
-    
+    private func setupSlider(_ slider: UISlider, color: UIColor) {
+        slider.value = 0
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = color
+        slider.maximumTrackTintColor = .gray
+        slider.thumbTintColor = color
+    }
 }
 
